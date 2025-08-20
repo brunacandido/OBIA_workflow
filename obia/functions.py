2e815fc46e7e9a062ae4378d11a2355a39415ea1
Beatriz Peres <beatriz@developmentseed.org>
2025-08-20 17:41:09 +0100

    few more segmentations

--- obia/functions.py
+++ obia/functions.py
@@ -2,7 +2,7 @@ import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage
-from skimage.segmentation import mark_boundaries, slic, watershed
+from skimage.segmentation import mark_boundaries, slic, watershed, felzenszwalb
 from skimage import filters
 from skimage.measure import regionprops_table, perimeter
 from skimage.util import map_array
@@ -32,14 +32,18 @@ def display_image(image):
     plt.axis('off')
     plt.show()
 
-def segmentation(image, segments, compactness, seg_mode, markers=2000):
+def segmentation(image, segments, compactness, seg_mode, 
+                 markers=2000,
+                 scale=3.0,
+                 sigma=0.95,
+                 min_size=50):
     # seg_mode can be:
     # - slic
     # - canny
     # - sobel
-    # - este
-    # - este
-    # - este
+    # - thrsehold_triangle
+    # - watershed
+    # - felz
     if seg_mode == "slic":
         segmentation = slic(image, 
                             segments, 
@@ -62,6 +66,36 @@ def segmentation(image, segments, compactness, seg_mode, markers=2000):
         sobel = skimage.filters.sobel(skimage.color.rgb2gray(image))
         segmentation = watershed(sobel,
                                  markers=markers)
+        boundaries = mark_boundaries(image, 
+                                     segmentation, 
+                                     color=(1, 0, 0), 
+                                     mode="thick")
+        
+    if seg_mode == "thrsehold_triangle":
+        opened = skimage.morphology.opening(skimage.color.rgb2gray(image), skimage.morphology.disk(1))
+        closed = skimage.morphology.closing(opened, skimage.morphology.disk(1))
+        thrsehold_triangle = closed < filters.threshold_triangle(closed)
+        segmentation = watershed(thrsehold_triangle,
+                                 markers=markers,
+                                 compactness=compactness)
+        boundaries = mark_boundaries(image, 
+                                     segmentation, 
+                                     color=(1, 0, 0), 
+                                     mode="thick")
+        
+    if seg_mode == "watershed":
+        segmentation = watershed(skimage.color.rgb2gray(image),
+                                 markers=markers)
+        boundaries = mark_boundaries(image, 
+                                     segmentation, 
+                                     color=(1, 0, 0), 
+                                     mode="thick")
+
+    if seg_mode == "felz":
+        segmentation = felzenszwalb(image,
+                                    scale=scale,
+                                    sigma=sigma,
+                                    min_size=min_size)
         boundaries = mark_boundaries(image, 
                                      segmentation, 
                                      color=(1, 0, 0), 
